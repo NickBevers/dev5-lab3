d1b924c9eb8bc1c909054508c2cb828f1a3db21c
nickbevers <nick.bevers9@gmail.com>
2022-09-29 22:30:35 +0200

    feat: add weather initialising to the main.js file to start the project

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-// import { setupCounter } from './counter.js'
+import Weather from './Weather';
 
-// https://www.fruityvice.com/api/fruit/ -> API to get fruits7
-// https://open-meteo.com/en/docs#latitude=50.8371&longitude=4.3676&hourly=temperature_2m -> API to get weather
-// https://api.open-meteo.com/v1/forecast?latitude=50.8371&longitude=4.3676&hourly=temperature_2m,rain,weathercode -> API to get weather better
+// https://www.fruityvice.com/api/fruit/ -> API to get fruits?
 
 document.querySelector('#app').innerHTML = `
   <h1 class="title">What fruit to eat today!</h1>
   <div class="add_container"> </div>
 `
+
+const weather = new Weather();
