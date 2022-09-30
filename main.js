46d785f6e9029727a98e48d1fbe428eb9c5ee46d
NickBevers <nick.bevers9@gmail.com>
2022-09-30 21:08:01 +0200

    fix: remove unneeded code to get a cleaner main.js

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
 import './style.css'
 import Weather from './Weather';
 
-// https://www.fruityvice.com/api/fruit/ -> API to get fruits?
-
-document.querySelector('#app').innerHTML = `
-  <h1 class="title">What fruit to eat today!</h1>
-  <div class="add_container"> </div>
-`
-
-const weather = new Weather();
+Weather.getWeather();
